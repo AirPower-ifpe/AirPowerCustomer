95c5889c2f1afc8a14213481f565a02cbe2b7c25
bill <wjefferson.santos@gmail.com>
2025-06-24 03:25:38 -0300

    [common] Improve Charts component
    [Desc]
     - Implements Charts contract strategy
     - Improve Charts component

--- common/src/main/java/com/ifpe/edu/br/common/components/Charts.kt
+++ common/src/main/java/com/ifpe/edu/br/common/components/Charts.kt
@@ -2,18 +2,16 @@ package com.ifpe.edu.br.common.components
 
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.ifpe.edu.br.common.ui.theme.ColorPrimaryDark
+import com.ifpe.edu.br.common.contracts.ChartDataWrapper
 import ir.ehsannarmani.compose_charts.ColumnChart
 import ir.ehsannarmani.compose_charts.models.BarProperties
-import ir.ehsannarmani.compose_charts.models.Bars
 
 /*
 * Trabalho de conclusão de curso - IFPE 2025
@@ -22,29 +20,32 @@ import ir.ehsannarmani.compose_charts.models.Bars
 */
 @Composable
 fun CustomBarChart(
-
+    paddingStart: Dp = 0.dp,
+    paddingTop: Dp = 0.dp,
+    paddingEnd: Dp = 0.dp,
+    paddingBottom: Dp = 0.dp,
+    height: Dp = 250.dp,
+    thickNes: Dp = 10.dp,
+    spacing: Dp = 5.dp,
+    dataWrapper: ChartDataWrapper
 ) {
+    val properties = BarProperties(thickNes, spacing)
     ColumnChart(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 22.dp),
+        modifier = Modifier
+            .height(height)
+            .padding(
+                start = paddingStart,
+                top = paddingTop,
+                end = paddingEnd,
+                bottom = paddingBottom
+            ),
+        barProperties = properties,
         data = remember {
-            listOf(
-                Bars(
-                    label = "1",
-                    values = listOf(
-                        Bars.Data(label = "aaa", value = 70.0, color = SolidColor(ColorPrimaryDark))
-                    ),
-                ),
-                Bars(
-                    label = "2",
-                    values = listOf(
-                        Bars.Data(label = "aaa", value = 60.0, color = SolidColor(ColorPrimaryDark))
-                    ),
-                )
-            )
+            dataWrapper.getDataSet().toBar()
         },
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
-        ),
+        )
     )
 }
