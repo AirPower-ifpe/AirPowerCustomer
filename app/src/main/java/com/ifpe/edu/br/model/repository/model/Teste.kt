ca99ebaab0b95110a451970f45ff4b77fdc28c65
Willian Santos <willjsan@motorola.com>
2025-07-10 10:24:44 -0300

    wip

---
+++ app/src/main/java/com/ifpe/edu/br/model/repository/model/Teste.kt
@@ -0,0 +1,10 @@
+package com.ifpe.edu.br.model.repository.model
+
+/*
+* Trabalho de conclusão de curso - IFPE 2025
+* Author: Willian Santos
+* Project: AirPower Costumer
+*/
+class Teste {
+val test: String = ""
+}
