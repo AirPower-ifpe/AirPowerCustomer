cf460582300c0ad621ddd15f84beae5d74384b9d
Willian Santos <wjsarq@gmail.com>
2026-09-15 15:16:04 -0300

    refactor: changes min sdk to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,10 +18,10 @@ android {
 
     defaultConfig {
         applicationId = "com.ifpe.edu.br"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0-alpha"
+        versionName = "2.0.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
